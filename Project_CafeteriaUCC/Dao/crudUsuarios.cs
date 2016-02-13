d5be4f4972447632b8b57c5830a1c6c444ad4d83
Christian Rodriguez <cristian010193@hotmail.com>
2016-02-13 01:26:27 -0500

    Version 1.3

---
+++ Project_CafeteriaUCC/Dao/crudUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    public interface crudUsuarios
+    {
+        int insert(Object obj);
+        int update(int identificacion, String nombre, String apellido, String direccion, String telefono_casa, String telefono_personal, String rol, String email, String imagen);
+        int delete(int id);
+        Object findById(int id);
+    }
+}
--- Project_CafeteriaUCC/Project_CafeteriaUCC/FormularioLogin.cs
+++ Project_CafeteriaUCC/Project_CafeteriaUCC/FormularioLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Control;
 
 namespace Project_CafeteriaUCC
 {
@@ -15,6 +16,18 @@ namespace Project_CafeteriaUCC
         public FormularioLogin()
         {
             InitializeComponent();
+        }
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            if (ctrlUsuario.insert(Convert.ToInt32(this.txt_usuario.Text), Convert.ToString(this.txt_contrasena.Text),"","","","","","","") > 0)
+            {
+                MessageBox.Show("Datos Guardados con Exito");
+            }
+            else
+            {
+                MessageBox.Show("Error al Guardar los Datos:");
+            }
         }
     }
 }
